03d3497b1c5c16bf1528da9340b1c95e5225da56
HobsonMC2105 <56050416+HobsonMC2105@users.noreply.github.com>
2019-10-02 15:03:44 -0700

    not finished

--- dice.py
+++ dice.py
@@ -1,3 +1,46 @@
-# Name:
-# Period
+# Name:Mathew Hobson	
+# Period 6
 # Dice Rolling Simulator
+
+import random
+
+
+rolls = int(input("How many times do you want to roll the dice?: "))
+
+x = 1
+
+a = 0
+b = 0
+c = 0
+d = 0
+e = 0
+f = 0
+
+
+while x <= rolls:
+	number = random.randint(1,6)
+	if number == 1:
+		a += 1
+	if number == 2:
+		b += 1
+	if number == 3:
+		c += 1
+	if number == 4:
+		d += 1
+	if number == 5:
+		e += 1
+	if number == 6:
+		f += 1
+	print("Roll " + str(x) + " is " + str(number))
+	x = x+1
+
+
+print("The results are:")
+print("Total rolls:" + str(rolls))
+print("1s: " + str(a))
+print("2s: " + str(b))
+print("3s: " + str(c))
+print("4s: " + str(d))
+print("5s: " + str(e))
+print("6s: " + str(f))
+
